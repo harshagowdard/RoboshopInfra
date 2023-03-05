ae55547fcf5c36eb1b90560fcb61f8c414169685
Harshavardhana Rukminiyamma Devegowda <harshavardhana.rukminiyamma.devegowda@sap.com>
2023-03-05 18:04:56 +0530

    Adding roboshop infra files

---
+++ ec2/main.tf
@@ -0,0 +1,68 @@
+data "aws_caller_identity" "current" {}
+
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "deveops-ansible-practice"
+  owners      = [data.aws_caller_identity.current.account_id]
+}
+
+resource "aws_instance" "ec2" {
+  ami = data.aws_ami.ami.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.sg.id]
+  tags = {
+    Name = var.component
+  }
+}
+
+resource "null_resource" "provisioner" {
+  provisioner "remote_exec" {
+    connection {
+      host = aws_instance.ec2.public_ip
+      user = "cento"
+      password = "DevOps321"
+    }
+
+    inline = [
+      "ansible-pull -i localhost, -U https://github.com/harshagowdard/RoboshopAnsible roboshop.yml -e role_name=${var.component}"
+    ]
+  }
+}
+
+resource "aws_security_group" "sg" {
+  name = "${var.component}-${var.env}-sg"
+  description = "All TLS inbound traffic"
+
+  ingress {
+    description = "ALL"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.component}-${var.env}-sg"
+  }
+}
+
+resource "aws_route53_record" "record" {
+  zone_id = "Z080164432BG98VPFPTT4"
+  name = "${var.component}.hdevegowda.online"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.ec2.private_ip]
+}
+
+variable "component" {}
+variable "instance_type" {}
+variable "env" {
+  default = "dev"
+}
